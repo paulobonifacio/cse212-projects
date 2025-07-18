59289fc19126ca6d2a6f17b60f4acb0799186af9
Paulo Bonifácio <paulo.bonifacio@live.com>
2025-07-18 22:38:47 +0200

    Update FeatureCollection.cs

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,23 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
 public class FeatureCollection
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    [JsonPropertyName("features")]
+    public List<Feature> Features { get; set; }
+}
+
+public class Feature
+{
+    [JsonPropertyName("properties")]
+    public FeatureProperties Properties { get; set; }
+}
+
+public class FeatureProperties
+{
+    [JsonPropertyName("place")]
+    public string Place { get; set; }
+
+    [JsonPropertyName("mag")]
+    public double? Mag { get; set; }
 }
